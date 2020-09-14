20d6b7f59546283c6fabad73b910a40a60533701
joao.romao <joao.romao@dataprev.gov.br>
2020-09-14 00:24:22 -0300

    Relacionamento Cíclico

--- src/test/java/br/ce/wcaquino/dbunit/ImportExport.java
+++ src/test/java/br/ce/wcaquino/dbunit/ImportExport.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.DatabaseConnection;
@@ -29,7 +33,41 @@ public class ImportExport {
 		DatabaseConnection dbConn = new DatabaseConnection(ConnectionFactory.getConnection());
 		FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
 		IDataSet dataSet = builder.build(new FileInputStream("massas" + File.separator + massa));
+		List<String> tabelas = obterTabelas();
+		desabiliarriggers(tabelas);
 		DatabaseOperation.CLEAN_INSERT.execute(dbConn, dataSet);
+		habilitarTriggers(tabelas);
+	}
+
+	private static void habilitarTriggers(List<String> tabelas) throws ClassNotFoundException, SQLException {
+		for (String tabela : tabelas) {
+			System.out.println("+++" + tabela);
+			Statement stmt = ConnectionFactory.getConnection().createStatement();
+			stmt.executeUpdate("ALTER TABLE public." + tabela + " enable trigger all");
+			stmt.close();
+		}
+
+	}
+
+	private static void desabiliarriggers(List<String> tabelas) throws ClassNotFoundException, SQLException {
+		for (String tabela : tabelas) {
+			System.out.println("---" + tabela);
+			Statement stmt = ConnectionFactory.getConnection().createStatement();
+			stmt.executeUpdate("ALTER TABLE public." + tabela + " disable trigger all");
+			stmt.close();
+		}
+
+	}
+
+	private static List<String> obterTabelas() throws ClassNotFoundException, SQLException {
+		List<String> tabelas = new ArrayList<String>();
+		ResultSet rs = ConnectionFactory.getConnection().createStatement()
+				.executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
+		while (rs.next()) {
+			tabelas.add(rs.getString("table_name"));
+		}
+		rs.close();
+		return tabelas;
 	}
 
 	public static void exportarBanco(String massa) throws Exception {
